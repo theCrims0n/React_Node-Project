2442af16c9896ef8eae6d6111868b914e09bcc02
theCrims0n <mi.salomon89@gmail.com>
2024-11-26 16:42:32 -0700

    commit

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize'
 
 const host = process.env.NODE_ENV == 'production' ? process.env.AWS_LINK : 'localhost'
 const port = process.env.NODE_ENV == 'production' ? process.env.AWS_PORT : 8080
-const key = process.env.NODE_ENV == 'production' ? process.env.AWS_SECRET_KEY! : 'root32'
+const key = process.env.NODE_ENV == 'production' ? process.env.AWS_SECRET_KEY_ : 'root32'
 
 const db = new Sequelize('nextia', 'postgres', key, {
     host: host,

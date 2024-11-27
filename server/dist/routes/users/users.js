9139998a9cbe6cdd8c196831a31a18c4353453fd
theCrims0n <mi.salomon89@gmail.com>
2024-11-26 17:36:26 -0700

    c

--- server/dist/routes/users/users.js
+++ server/dist/routes/users/users.js
@@ -4,10 +4,10 @@ const express_1 = require("express");
 const users_1 = require("../../controller/users/users");
 const { validateJWT } = require('../../helper/jwt');
 const router = (0, express_1.Router)();
-router.get('/', users_1.getUsers);
-router.post('/pagination', users_1.getUsersPagination);
-router.get('/:id', users_1.getUsersById);
-router.put('/', users_1.editUser);
-router.delete('/:id', users_1.deteleteUser);
+router.get('/', validateJWT, users_1.getUsers);
+router.post('/pagination', validateJWT, users_1.getUsersPagination);
+router.get('/:id', validateJWT, users_1.getUsersById);
+router.put('/', validateJWT, users_1.editUser);
+router.delete('/:id', validateJWT, users_1.deteleteUser);
 exports.default = router;
 //# sourceMappingURL=users.js.map

ad0ca531647c233623b94366f76ab8a2f6bd13b0
theCrims0n <mi.salomon89@gmail.com>
2024-11-27 10:30:33 -0700

    c

--- server/dist/models/server.js
+++ server/dist/models/server.js
@@ -41,7 +41,7 @@ class Server {
     }
     middlewares() {
         this.app.use(express_1.default.json());
-        this.app.use((0, cors_1.default)({ credentials: true, origin: 'http://localhost:3000' }));
+        this.app.use((0, cors_1.default)({ credentials: true, origin: 'https://react-node-project-kappa.vercel.app' }));
         this.app.use(express_1.default.urlencoded({ extended: true }));
         this.app.use(express_1.default.static('public'));
         this.app.use((0, cookie_parser_1.default)());

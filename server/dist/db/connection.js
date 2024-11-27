e9d5df87367bf060f5df168d887a20fe1ee12cb8
theCrims0n <mi.salomon89@gmail.com>
2024-11-26 17:08:54 -0700

    commit

--- server/dist/db/connection.js
+++ server/dist/db/connection.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const host = process.env.NODE_ENV == 'production' ? process.env.AWS_LINK : 'localhost';
 const port = process.env.NODE_ENV == 'production' ? process.env.AWS_PORT : 8080;
-const key = process.env.NODE_ENV == 'production' ? process.env.AWS_SECRET_KEY : 'root32';
+const key = process.env.NODE_ENV == 'production' ? process.env.AWS_SECRET_KEY_ : 'root32';
 const db = new sequelize_1.Sequelize('nextia', 'postgres', key, {
     host: host,
     port: Number(port),

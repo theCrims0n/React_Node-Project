75188843d86f2b300b1d8f35148d71a32697c66e
theCrims0n <mi.salomon89@gmail.com>
2024-11-26 17:15:18 -0700

    c

--- server/models/server.ts
+++ server/models/server.ts
@@ -34,7 +34,8 @@ class Server {
 
     middlewares() {
         this.app.use(express.json())
-        this.app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+        this.app.use(cors())
+        //this.app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
         this.app.use(express.urlencoded({ extended: true }))
         this.app.use(express.static('public'))
         this.app.use(cookieParser())

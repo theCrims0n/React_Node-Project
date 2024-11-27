d7a58686eb34cedf4a55414cba5bfc74eb9932ca
theCrims0n <mi.salomon89@gmail.com>
2024-11-27 12:11:41 -0700

    Update server.ts

--- server/models/server.ts
+++ server/models/server.ts
@@ -34,7 +34,7 @@ class Server {
 
     middlewares() {
         this.app.use(express.json())
-        this.app.use(cors({ credentials: true, origin: 'https://react-node-project-kappa.vercel.app' }))
+        this.app.use(cors({ credentials: true, origin: 'https://react-node-project-client.vercel.app' }))
         this.app.use(express.urlencoded({ extended: true }))
         this.app.use(express.static('public'))
         this.app.use(cookieParser())

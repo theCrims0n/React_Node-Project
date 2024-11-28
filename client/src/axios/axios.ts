24f0cdbc7484c7f1543d3bcd7d6f8864668c35f7
theCrims0n <mi.salomon89@gmail.com>
2024-11-27 22:24:07 -0700

    Update axios.ts

--- client/src/axios/axios.ts
+++ client/src/axios/axios.ts
@@ -4,7 +4,7 @@ export default axios.create({
     baseURL: 'https://nodejs-serverless-function-express-al44.onrender.com',
     ///baseURL: 'http://localhost:3001',
     headers: {
-        'Access-Control-Allow-Origin': 'https://react-node-project-client-l5o45bwqd-thecrims0ns-projects.vercel.app',
+        'Access-Control-Allow-Origin': 'https://react-node-project-client-ayoyd0jc1-thecrims0ns-projects.vercel.app/',
         'Access-Control-Max-Age': '1800',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
         'Access-Control-Allow-Headers': 'Content-Type',

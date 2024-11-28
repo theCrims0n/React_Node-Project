efcde16db2b6e2f71211f2ca6ed2d92b14c4e270
theCrims0n <mi.salomon89@gmail.com>
2024-11-28 14:00:21 -0700

    c

--- client/src/axios/axios.ts
+++ client/src/axios/axios.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 axios.defaults.withCredentials = true
 
 export default axios.create({
-    baseURL: 'https://nodejs-serverless-function-express-al44.onrender.com',
+    baseURL: 'https://react-node-project-rqif.onrender.com',
     //baseURL: 'http://localhost:3001',
     headers: {
-        'Access-Control-Allow-Origin': 'https://react-node-project-client.vercel.app',
+        'Access-Control-Allow-Origin': '*',
         //'Access-Control-Allow-Origin': 'http://localhost:3000',
         'Access-Control-Max-Age': '1800',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',

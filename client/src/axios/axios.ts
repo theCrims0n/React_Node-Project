e41f63a67358cc5b4d697688c2e520efab040b81
theCrims0n <mi.salomon89@gmail.com>
2024-11-28 16:04:37 -0700

    c

--- client/src/axios/axios.ts
+++ client/src/axios/axios.ts
@@ -6,7 +6,7 @@ export default axios.create({
     baseURL: 'https://react-node-project-rqif.onrender.com',
     //baseURL: 'http://localhost:3001',
     headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'https://react-node-project-1-my5f.onrender.com',
         //'Access-Control-Allow-Origin': 'http://localhost:3000',
         'Access-Control-Max-Age': '1800',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',

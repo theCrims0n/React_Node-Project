15131b6a4527fe89787d38426eb91fd50021c56a
theCrims0n <mi.salomon89@gmail.com>
2024-11-27 21:47:54 -0700

    Update axios.ts

--- client/src/axios/axios.ts
+++ client/src/axios/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://nodejs-serverless-function-express-al44.onrender.com',
     headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:https://react-node-project-client.vercel.app',
+        'Access-Control-Allow-Origin': 'https://react-node-project-client.vercel.app',
         'Access-Control-Max-Age': '1800',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
         'Access-Control-Allow-Headers': 'Content-Type',

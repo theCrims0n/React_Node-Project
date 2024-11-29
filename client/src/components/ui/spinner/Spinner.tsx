70b934d5493019eacd0ec36dcc610850c0daafee
theCrims0n <mi.salomon89@gmail.com>
2024-11-29 01:03:58 -0700

    c

--- client/src/components/ui/spinner/Spinner.tsx
+++ client/src/components/ui/spinner/Spinner.tsx
@@ -2,7 +2,7 @@ export default function Spinner() {
 
     return (
         <div className="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2">
-            <div className="animate-spin inline-block size-12 border-[5px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500" role="status" aria-label="loading">
+            <div className="animate-spin inline-block size-14 border-[5px] border-current border-t-transparent text-slate-800 rounded-full dark:text-slate-700" role="status" aria-label="loading">
                 <span className="sr-only">Loading...</span>
             </div>
         </div>
--- client/src/pages/invitations/list/List.tsx
+++ client/src/pages/invitations/list/List.tsx
@@ -1,126 +1,111 @@
-import { useEffect, useState } from "react"
-import Spinner from "../../../components/ui/spinner/Spinner"
-import { useUsersStore } from "../../../store/users/users"
-import { Link, useLocation, useParams } from "react-router-dom"
-import { Pencil, RefreshCcw, Trash2 } from "lucide-react"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { Pencil, Trash2 } from "lucide-react"
 import { Pagination } from "../../../components/ui/pagination/Pagination"
-import UsePagination from "../../../store/pagation/UsePagination"
-import { Tooltip } from 'react-tooltip'
-import { RefreshButton } from "../../../components/ui/refresh/RefreshButton"
+import { DialogModal } from "../../../components/ui/dialog/Dialog"
+import { useModalStore } from "../../../store/ui/modal"
+import { usePaginationStore } from "../../../store/pagination-store/pagination"
+import SpinnerCard from "../../../components/ui/spinner/Spinner-Card"
 
 const InvitationsList = () => {
 
-    const { page }: any = useParams()
-    const { isLoading: isLoadingUsers, deleteUser } = useUsersStore()
-    const [totalPages, setTotalPages] = useState(0)
-    const [isLoading, setIsLoading] = useState(true)
-    const [data, setData] = useState<any[]>([])
+    const { openModalMenu } = useModalStore()
+    const { data, isLoadingPagination } = usePaginationStore()
     const [idDelete, setIdDelete] = useState(0)
 
-    useEffect(() => {
+    return (
+        <div className="flex flex-col">
+            <div className="min-h-96">
 
-        getPagination(page)
-
-    }, [page, isLoadingUsers])
-
-    const getPagination = async (page: number) => {
-        const url = '/api/users'
-        const { totalPages, data } = await UsePagination({ page, url })
-        setTotalPages(totalPages)
-        setData(data)
-        setIsLoading(false)
-        setTimeout(() => {
-            setIdDelete(0)
-        }, 500);
-    }
-
-    return (<>
-        {
-            isLoading
-                ?
-                <Spinner />
-                :
-                <div >
-                    <Link to={'/users/register'}><button className="m-8 button">Create new invitation</button></Link>
-                    <div className="min-h-80 fade-in m-10 relative flex flex-col overflow-auto text-slate-700 bg-white shadow-md rounded-xl bg-clip-border">
-                        <table className=" fade-in w-full text-left table-auto min-w-max">
-                            <thead>
-                                <tr className="font-bold">
-                                    <th className="p-4 border-b border-blue-slate-100 bg-blue-slate-50">
-                                        <p className="block antialiased leading-none text-blue-slate-900 ">
-                                            Name
-                                        </p>
-                                    </th>
-                                    <th className="p-4 border-b border-blue-slate-100 bg-blue-slate-50">
-                                        <p className="block antialiased  leading-none text-blue-slate-900 ">
-                                            Last Name
-                                        </p>
-                                    </th>
-                                    <th className="p-4 border-b border-blue-slate-100 bg-blue-slate-50">
-                                        <p className="block antialiased  leading-none text-blue-slate-900 ">
-                                            Email
-                                        </p>
-                                    </th>
-                                    <th className="p-4 border-b border-blue-slate-100 bg-blue-slate-50">
-                                        <p className="block antialiased  leading-none text-blue-slate-900 ">
-                                            Department
-                                        </p>
-                                    </th>
-                                    <th className="p-4 border-b border-blue-slate-100 bg-blue-slate-50">
-                                        <p className="block antialiased  leading-none text-blue-slate-900 ">Actions</p>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    data.map((user, index) => {
-                                        return (
-                                            <tr key={index} className={user.id == idDelete ? 'opacity-0 transition-all duration-500' : ''}>
-                                                <td className="p-4 border-b border-blue-slate-50">
-                                                    <p className="block antialiased  leading-normal text-blue-slate-900">
-                                                        {user.name}
-                                                    </p>
-                                                </td>
-                                                <td className="p-4 border-b border-blue-slate-50">
-                                                    <p className="block antialiased  leading-normal text-blue-slate-900">
-                                                        {user.lastname}
-                                                    </p>
-                                                </td>
-                                                <td className="p-4 border-b border-blue-slate-50">
-                                                    <p className="block antialiased  leading-normal text-blue-slate-900">
-                                                        {user.email}
-                                                    </p>
-                                                </td>
-                                                <td className="p-4 border-b border-blue-slate-50">
-                                                    <p className="block antialiased  leading-normal text-blue-slate-900">
-                                                        {user.department}
-                                                    </p>
-                                                </td>
-                                                <td className="py-4 flex space-x-8 ...">
-                                                    <div>
-                                                        <Link x-data="{ tooltip: 'Edit' }" to={`/users/edit/${user.id}`} className="cursor-pointer">
-                                                            <Pencil color="#366a0c" />
-                                                        </Link>
-                                                    </div>
-                                                    <div >
-                                                        <a x-data="{ tooltip: 'Delete' }" onClick={() => [setIdDelete(user.id), deleteUser(user.id)]} className="cursor-pointer">
-                                                            <Trash2 color="#920c0c" />
-                                                        </a>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="flex flex-wrap justify-center mt-10">
-                        <Pagination url={'/api/invitations'} />
-                    </div>
-                </div >
-        }
-    </>)
+                <DialogModal value={idDelete} title="Delete user" content="Are you sure you want to delete this user? All of its data will be permanently removed.
+                                                This action cannot be undone." />
+                <Link to={'/users/register'}><button className="m-8 button">Create new user</button></Link>
+                <div className="justify-center min-h-96 m-10 flex flex-col overflow-auto text-slate-700 bg-white shadow-md rounded-xl bg-clip-border">
+                    <table className="w-full text-left table-auto min-w-max">
+                        {
+                            isLoadingPagination
+                                ?
+                                <SpinnerCard />
+                                :
+                                <>
+                                    <thead className="font-medium">
+                                        <tr>
+                                            <th className="p-4 border-b border-blue-slate-100 bg-blue-slate-50">
+                                                <p className="font-medium block leading-none text-blue-slate-900 ">
+                                                    Name
+                                                </p>
+                                            </th>
+                                            <th className="p-4 border-b border-blue-slate-100 bg-blue-slate-50">
+                                                <p className="block font-medium  leading-none text-blue-slate-900 ">
+                                                    Last Name
+                                                </p>
+                                            </th>
+                                            <th className="p-4 border-b border-blue-slate-100 bg-blue-slate-50">
+                                                <p className="block font-medium  leading-none text-blue-slate-900 ">
+                                                    Email
+                                                </p>
+                                            </th>
+                                            <th className="p-4 border-b border-blue-slate-100 bg-blue-slate-50">
+                                                <p className="block  font-medium leading-none text-blue-slate-900 ">
+                                                    Department
+                                                </p>
+                                            </th>
+                                            <th className="p-4 border-b border-blue-slate-100 bg-blue-slate-50">
+                                                <p className="block  font-medium leading-none text-blue-slate-900 ">Actions</p>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody >
+                                        {
+                                            data.map((user, index) => {
+                                                return (
+                                                    <tr key={index} className="hover:bg-slate-100 font=normal text-sm">
+                                                        <td className="p-4 border-b border-blue-slate-50">
+                                                            <p className="block  text-slate-800 ">
+                                                                {user.name}
+                                                            </p>
+                                                        </td>
+                                                        <td className="p-4 border-b border-blue-slate-50">
+                                                            <p className="block  text-slate-800">
+                                                                {user.lastname}
+                                                            </p>
+                                                        </td>
+                                                        <td className="p-4 border-b border-blue-slate-50">
+                                                            <p className="block  text-slate-800">
+                                                                {user.email}
+                                                            </p>
+                                                        </td>
+                                                        <td className="p-4 border-b border-blue-slate-50">
+                                                            <p className="block text-slate-800">
+                                                                {user.department}
+                                                            </p>
+                                                        </td>
+                                                        <td className="py-4 flex space-x-8 ...">
+                                                            <div>
+                                                                <Link x-data="{ tooltip: 'Edit' }" to={`/users/edit/${user.id}`} className="cursor-pointer">
+                                                                    <Pencil color="#366a0c" />
+                                                                </Link>
+                                                            </div>
+                                                            <div >
+                                                                <button type="button" onClick={() => [openModalMenu(), setIdDelete(user.id)]} className="cursor-pointer block">
+                                                                    <Trash2 color="#920c0c" />
+                                                                </button>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            })
+                                        }
+                                    </tbody>
+                                </>}
+                    </table>
+                </div>
+            </div>
+            <div className="flex flex-wrap justify-center mt-1">
+                <Pagination url={'/api/users'} />
+            </div>
+        </div>
+    )
 }
 
 export default InvitationsList

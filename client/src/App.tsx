2150892ddc3026c73e25778c78a77cb12421ed50
theCrims0n <mi.salomon89@gmail.com>
2024-11-28 20:09:27 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -35,7 +35,7 @@ const App = () => {
   }
 
   return (
-    <div key={token || Number(isAuthentic)} className='fade-in'>
+    <div key={token} className='fade-in'>
       <AuthProvider>
         <Routes>
           <Route element={<AuthPages />}>

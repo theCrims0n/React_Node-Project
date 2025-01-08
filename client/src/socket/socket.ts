6f2ff879e9cef5476d08ba65e590bb6030a2cce7
theCrims0n <mi.salomon89@gmail.com>
2025-01-07 19:36:47 -0700

    c
    c

--- client/src/socket/socket.ts
+++ client/src/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:3001';
+const URL = 'https://react-node-project-rqif.onrender.com';
 
 export const socket = io(URL, {
     reconnection: true,
